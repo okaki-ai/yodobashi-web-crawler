ec6b73089144eff38cb6917aa400e2e1256e81ed
unknown <yoooooosuke48@gmail.com>
2021-11-29 23:00:18 +0900

    fixed a bug

--- yodobashi_scrayping.py
+++ yodobashi_scrayping.py
@@ -1,32 +1,58 @@
 import time
 import requests
+import csv
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import chromedriver_binary 
 
+url = "https://www.yodobashi.com/category/6353/6357/6407/6636/" # 初期ページのURLを格納
+# url = "https://www.yodobashi.com/category/6353/6357/6407/500000185000/"
+res = [["#", "メーカー", "商品名"]] # 結果格納用
+i = 0 # 結果ID用
 driver = webdriver.Chrome()
-driver.get("https://www.yodobashi.com/category/6353/6357/6407/6636/")
+driver.get(url)
 
 time.sleep(2)
 
+# 初期ページ
 product_list = driver.find_elements_by_css_selector("div.srcResultItem.spt_hznList.tileTypeList.js_productList.changeTile")
+
 p = product_list[0]
 product_name = p.find_elements_by_css_selector(".pName.fs14")
-# print(product_name.text)
-# maker = p.find_elements_by_css_selector(".pSubInfo.pcParts")
-res = {}
-i = 0
+
+
 for pn in product_name:
     i = i+1
     list_product = pn.text.splitlines()
-    d1 = {i: list_product}
-    res.update(d1)
+    d1 = [i, list_product[0], list_product[1]]
+    res.append(d1)
 
 print(res) 
 
+# 2ページ以降
+page_num = 1
+while True:
+    try:
+        page_num =page_num + 1 
+        url1 = url + "p" + str(page_num)
+        driver.get(url1)
+        product_list = driver.find_elements_by_css_selector("div.srcResultItem.spt_hznList.tileTypeList.js_productList.changeTile")
+        p = product_list[0]
+        product_name = p.find_elements_by_css_selector(".pName.fs14")
 
-driver.quit()
+        for pn in product_name:
+            i = i+1
+            list_product = pn.text.splitlines()
+            d1 = [i, list_product[0], list_product[1]]
+            res.append(d1)
+        print(res)
 
+    except:
+        driver.quit()
+        break
 
-#listContents > div.srcResultItem.spt_hznList.tileTypeList.js_productList.changeTile//*[@id="listContents"]/div[3]/div[1]/a/div[2]/p[1]
+f = open('out.csv', 'w', newline='')
+writer = csv.writer(f)
+writer.writerows(res)
+f.close()
